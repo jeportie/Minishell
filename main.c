c1eb7750e88d63e9e34866d0ecee35da196d3e09
Jerome Portier <jeportie@made-f0Cr1s3.clusters.42paris.fr>
2024-09-24 12:06:28 +0200

    restructuration on the files -> START PROJECT

---
+++ main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 11:52:47 by jeportie          #+#    #+#             */
+/*   Updated: 2024/09/24 12:04:59 by jeportie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+ *TODO: Step 1: 
+ *->Files creation
+ *->structure 
+ *->open minishell and display a prompt
+ *->first test unit for main and diplsay function
+ *->Coding + Test green
+ *->Refactoring
+ *->Next Test coding
+ *->End step one
+ */
+
+int main(void)
+{
+	
+	return (0);
+}
