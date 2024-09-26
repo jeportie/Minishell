43fbe99ed577e5c78cc05605e829e564e231ea75
jeportie <jeromevdb.production@gmail.com>
2024-09-26 14:09:45 +0000

    update

--- test_src/CTestTestfile.cmake
+++ test_src/CTestTestfile.cmake
@@ -4,7 +4,7 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(test_ms_get_user_input "/root/projects/minishell/test_src/test_ms_get_user_input")
-set_tests_properties(test_ms_get_user_input PROPERTIES  TIMEOUT "5" _BACKTRACE_TRIPLES "/root/projects/minishell/test_src/CMakeLists.txt;37;add_test;/root/projects/minishell/test_src/CMakeLists.txt;0;")
-add_test(test_ms_init_shell "/root/projects/minishell/test_src/test_ms_init_shell")
-set_tests_properties(test_ms_init_shell PROPERTIES  TIMEOUT "5" _BACKTRACE_TRIPLES "/root/projects/minishell/test_src/CMakeLists.txt;37;add_test;/root/projects/minishell/test_src/CMakeLists.txt;0;")
+add_test(ms_get_user_input "/root/projects/minishell/test_src/test_ms_get_user_input")
+set_tests_properties(ms_get_user_input PROPERTIES  TIMEOUT "5" _BACKTRACE_TRIPLES "/root/projects/minishell/test_src/CMakeLists.txt;40;add_test;/root/projects/minishell/test_src/CMakeLists.txt;0;")
+add_test(ms_init_shell "/root/projects/minishell/test_src/test_ms_init_shell")
+set_tests_properties(ms_init_shell PROPERTIES  TIMEOUT "5" _BACKTRACE_TRIPLES "/root/projects/minishell/test_src/CMakeLists.txt;40;add_test;/root/projects/minishell/test_src/CMakeLists.txt;0;")
