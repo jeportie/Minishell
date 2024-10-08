060bff649cb2246cebee4bcbc7e835740a725fd5
jeportie <jeromevdb.production@gmail.com>
2024-10-08 10:42:12 +0000

    first push ast branch

---
+++ include/ast.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ast.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 08:45:53 by jeportie          #+#    #+#             */
+/*   Updated: 2024/10/08 10:41:44 by jeportie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AST_H
+# define AST_H
+
+typedef enum
+{
+	NODE_COMMAND,
+	NODE_PIPE,
+	NODE_REDIRECT_IN,
+	NODE_REDIRECT_OUT,
+	NODE_REDIRECT_APPEND,
+	NODE_REDIRECT_HEREDOC,
+	NODE_AND,
+	NODE_OR,
+	NODE_SEQUENCE,
+	NODE_BACKGROUND,
+	NODE_SUBSHELL
+} e_node_type;
+
+typedef struct s_cmd_node
+{
+    char **argv;              // Command and arguments
+    int argc;                 // Argument count
+}				t_cmd_node;
+
+
+#endif /*AST_H*/
