b60906086a9d5a1c640c524b0aaf2a1cd64062c4
jeportie <jeromevdb.production@gmail.com>
2024-10-14 22:15:24 +0000

    push pause

--- obj/env_lst_utils/ms_env_create_node.d
+++
@@ -1,8 +0,0 @@
-obj/env_lst_utils/ms_env_create_node.o: \
- src/env_lst_utils/ms_env_create_node.c \
- src/env_lst_utils/../../include/minishell.h \
- src/env_lst_utils/../../include/../lib/libgc/include/libgc.h \
- src/env_lst_utils/../../include/../lib/libft/include/libft.h
-src/env_lst_utils/../../include/minishell.h:
-src/env_lst_utils/../../include/../lib/libgc/include/libgc.h:
-src/env_lst_utils/../../include/../lib/libft/include/libft.h:
