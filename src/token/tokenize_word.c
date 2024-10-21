2c29f37d3e9019f4fac4a7d9de543575af076da3
jeportie <jeromevdb.production@gmail.com>
2024-10-21 16:09:55 +0000

    push pause

--- src/token/tokenize_word.c
+++ src/token/tokenize_word.c
@@ -6,11 +6,16 @@
 /*   By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 14:41:20 by jeportie          #+#    #+#             */
-/*   Updated: 2024/10/21 15:33:27 by jeportie         ###   ########.fr       */
+/*   Updated: 2024/10/21 16:09:52 by jeportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/tokenize.h"
+
+/*
+ * NOTE:
+ *	if empty '' or "" token == ''
+ */
 
 t_token	*tokenize_word(const char **input, t_gc *gcl)
 {
@@ -29,7 +34,7 @@ t_token	*tokenize_word(const char **input, t_gc *gcl)
 	current = (char *)*input;
 	len = 0;
 	while (current[len] && !is_whitespace(current[len])
-			&& !is_operator(current[len]) && !is_quote(current[len])
+			&& !is_operator(current[len])// && !is_quote(current[len])
 			&& !is_frame(current[len]))
 		len++;
 	value = (char *)gc_malloc(sizeof(char) * len + 1, gcl);
