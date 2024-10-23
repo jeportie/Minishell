696503419d1a08312e6366d8f2d3e836f1d66c2c
jeportie <jeromevdb.production@gmail.com>
2024-10-23 17:51:13 +0000

    push pause

--- src/syntax_error/ms_syntax_error.c
+++ src/syntax_error/ms_syntax_error.c
@@ -6,12 +6,11 @@
 /*   By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 12:11:22 by jeportie          #+#    #+#             */
-/*   Updated: 2024/10/23 16:49:10 by jeportie         ###   ########.fr       */
+/*   Updated: 2024/10/23 17:49:46 by jeportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/syntax.h"
-
 
 static int	st_quote_error(const char *input)
 {
@@ -26,6 +25,10 @@ static int	st_quote_error(const char *input)
 	int		o_and;
 	int		o_or;
 	int		o_pipe;
+	int		r_in;
+	int		r_out;
+	int		r_app;
+	int		r_here;
 
 	current = ft_strtrim((char *)input, " ");
 	len_end = ft_strlen(current) - 1;
@@ -38,13 +41,76 @@ static int	st_quote_error(const char *input)
 	o_and = 0;
 	o_or = 0;
 	o_pipe = 0;
+	r_in = 0;
+	r_out = 0;
+	r_app = 0;
+	r_here = 0;
 	while (current[i])
 	{
-		if (current[i] != ' ' && current[i] != '(' && current[i] != '&' && current[i] != '|' && ft_isascii(current[i]))
+		if (current[i] != ' ' && current[i] != '(' && current[i] != ')' && current[i] != '&' && current[i] != '|' && current[i] != '<' && current[i] != '>'&& ft_isascii(current[i]))
+		{
+			r_in = 0;
+			r_here = 0;
+			r_out = 0;
+			r_app = 0;
+		}
+		if (current[i] != ' ' && current[i] != '(' && current[i] != ')' && current[i] != '&' && current[i] != '|' && ft_isascii(current[i]))
 		{
 			o_and = 0;
 			o_or = 0;
 			o_pipe = 0;
+		}
+		if (current[i] == '&' && o_and)
+		{
+			error = 6;
+			break ;
+		}
+		if (!dq_trigger && !sq_trigger && (current[i] == '<' || current[i] == '>'))
+		{
+			if (current[i] == '<')
+			{
+				if (current[i + 1] && current[i + 1] == '<')
+				{
+					if (r_in || r_here || r_out || r_app)
+					{
+						error = 3;
+						break ;
+					}
+					i++;
+					r_here = 1;
+				}
+				else
+				{
+					if (r_in || r_here || r_out || r_app)
+					{
+						error = 2;
+						break ;
+					}
+					r_in = 1;
+				}
+			}
+			else if (current[i] == '>')
+			{
+				if (current[i + 1] && current[i + 1] == '>')
+				{
+					if (r_in || r_here || r_out || r_app)
+					{
+						error = 5;
+						break ;
+					}
+					i++;
+					r_app = 1;
+				}
+				else
+				{
+					if (r_in || r_here || r_out || r_app)
+					{
+						error = 4;
+						break ;
+					}
+					r_out = 1;
+				}
+			}
 		}
 		if (current[i] == '\'' && sq_trigger)
 			sq_trigger = 0;
@@ -58,6 +124,11 @@ static int	st_quote_error(const char *input)
 		{ 
 			if (p_trigger == 1)
 				break ;
+			else if (r_in || r_here || r_out || r_app)
+			{
+				p_trigger = 1;
+				break ;
+			}
 			else if (i == 0 || o_and || o_or || o_pipe)
 			{
 				p_char = 0;
@@ -74,8 +145,11 @@ static int	st_quote_error(const char *input)
 		}
 		else if (current[i] == ')')
 		{
-			if (!dq_trigger && !sq_trigger && p_trigger && p_char > 0)
-				p_trigger = 0;
+			if (r_in || r_here || r_out || r_app)
+			{
+				p_trigger = 2;
+				break ;
+			}
 			else if (!dq_trigger && !sq_trigger && p_trigger && p_char == 0)
 			{
 				p_trigger = 2;
@@ -86,49 +160,70 @@ static int	st_quote_error(const char *input)
 				p_trigger = 2;
 				break ;
 			}
+			else if (o_and || o_or || o_pipe)
+			{
+				p_trigger = 2;
+				break ;
+			}
+			else if (!dq_trigger && !sq_trigger && p_trigger && p_char > 0)
+				p_trigger = 0;
 		}
 		else if (!dq_trigger && !sq_trigger && (current[i] == '&' || current[i] == '|'))
 		{
 			if (current[i] == '&' && current[i + 1] && current[i + 1] == '&')
 			{
+				if (r_in || r_here || r_out || r_app)
+				{
+					error = 7;
+					break ;
+				}
 				if (o_and || o_or || o_pipe)
 				{
-					error = 3;
+					error = 7;
 					break ;
 				}
 				if (i == 0)
-					error = 3;
+					error = 7;
 				i++;
-				printf("i = %d|end = %d\n", i, len_end);
 				if (i == len_end)
-					error = 3;
+					error = 7;
 				o_and = 1;
 			}
 			else if (current[i] == '|' && current[i + 1] && current[i + 1] == '|')
 			{
+				if (r_in || r_here || r_out || r_app)
+				{
+					error = 8;
+					break ;
+				}
 				if (o_and || o_or || o_pipe)
 				{
-					error = 3;
+					error = 8;
 					break ;
 				}
 				if (i == 0)
-					error = 3;
+					error = 8;
 				i++;
 				if (i == len_end)
-					error = 3;
+					error = 8;
 				o_or = 1;
 			}
 			else if (current[i] == '|')
 			{
+				if (r_in || r_here || r_out || r_app)
+				{
+					error = 9;
+					break ;
+				}
 				if (o_and || o_or || o_pipe)
 				{
-					error = 3;
+					error = 9;
 					break ;
 				}
 				if (i == 0)
-					error = 3;
+					error = 9;
 				if (i == len_end)
-					error = 3;
+					error = 9;
 				o_pipe = 1;
 			}
 		}
@@ -138,8 +233,22 @@ static int	st_quote_error(const char *input)
 	}
 	if (p_trigger > 0 || sq_trigger || dq_trigger || error > 0)
 	{
+		if (error == 2)
+			printf("Minishell: syntax error near unexpected token `<'\n");
 		if (error == 3)
-			printf("Minishell: syntax error operator\n");
+			printf("Minishell: syntax error near unexpected token `<<'\n");
+		if (error == 4)
+			printf("Minishell: syntax error near unexpected token `>'\n");
+		if (error == 5)
+			printf("Minishell: syntax error near unexpected token `>>'\n");
+		if (error == 6)
+			printf("Minishell: syntax error near unexpected token `&'\n");
+		if (error == 7)
+			printf("Minishell: syntax error near unexpected token `&&'\n");
+		if (error == 8)
+			printf("Minishell: syntax error near unexpected token `||'\n");
+		if (error == 9)
+			printf("Minishell: syntax error near unexpected token `|'\n");
 		error = 1;
 		if (sq_trigger == 1)
 			printf("Minishell: syntax error near unexpected token `\''\n");
